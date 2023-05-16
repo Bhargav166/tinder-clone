1839ebcf6fdfdb955b8d2c1ab69d337ac4c20978
Bhargav166 <solankibhargav2013@gmail.com>
2023-05-16 20:36:02 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+        <Routes>
+          {/* Header */}
+          <Route index path='/' element={<Header />} />
+
+          {/* Tinder Cards */}
+          {/* Buttons below tinder cards */}
+
+          {/* Chats screen */}
+          {/* Individual chats of people */}
+        </Routes>
+      </BrowserRouter>
+
+
+    </div >
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import './Header.css'
+import TinderLogo from './images/tinder_logo.png'
+import IconButton from '@mui/material/IconButton';
+
+function Header() {
+    return (
+        <div className='header'>
+            <IconButton>
+                <PersonIcon className='header_icons' fontSize='large' />
+            </IconButton>
+            <img src={TinderLogo} alt="tinder logo" className='tinder__logo' />
+
+            <IconButton>
+                <ForumIcon className='header_icons' fontSize='large' />
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
